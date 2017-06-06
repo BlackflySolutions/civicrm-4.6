ee7ca22f82de6426588768e6134b942603b8274f
Drupal Local User <drupal@civicrm.ca>
2017-06-06 11:49:56 -0400

    Updated to 4.6.28

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitad275075a062e835f3aef6c8a106741a::getLoader();
+return ComposerAutoloaderInit15273363a8ff4b798eb304902c5f2b23::getLoader();
